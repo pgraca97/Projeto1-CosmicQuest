4bb646f654a7b9061b5e9fc2a6897a26a45b9688
pgraca97 <116584573+pgraca97@users.noreply.github.com>
2023-06-18 07:05:33 +0100

    Bonus Trivia Implementation, Coins System Addition, and Shopkeeper Initiation
    This commit brings several significant advancements to our project:
    
    1. The alphabet soup bonus trivia challenge is now implemented. While the current algorithm serves its purpose well, potential enhancements could be made in the future if time allows.
    
    2. The coins system has been integrated into the game. This new feature adds an extra layer of engagement and reward for the players.
    
    3. The implementation of the shopkeeper component has begun. This addition is expected to further enrich the gameplay by providing more interactive elements for the players.
    
    While there's still work to be done, substantial progress has been made. The goal is to have all elements finished and polished in the next day or two.

--- js/Model/Quizz.js
+++ js/Model/Quizz.js
@@ -1,15 +1,13 @@
 export default class Quizz {
-    // The constructor is called when a new instance of Challenge is created.
-    // The type, choices, question and answer are passed in when the new Challenge is created.
-    constructor(type, choices, question, answer) {
-        this.type = type;  // The type of challenge: 'multiple choice', 'short answer', 'fill in the blanks', 'alphabet soup'
-        this.question = question;  // The question text
-        this.answer = answer;  // The correct answer
-        this.isAnsweredCorrectly = null;  // Whether the challenge is answered correctly or not
+    constructor(type, choices, question, answer, hints = []) {
+        this.type = type;
+        this.question = question;
+        this.answer = answer;
+        this.isAnsweredCorrectly = null;
         this.choices = choices;
-        
+        this.hints = hints;  // Save the hints array
         if (type == 'alphabet soup') {
-            this.alphabetSoup = []; // Will hold the alphabet soup clicked letters 
+            this.alphabetSoup = [];
         }
     }
 }
